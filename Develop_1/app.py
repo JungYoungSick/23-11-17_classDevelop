3da08f5183b8d9e7082a5da9466a6135bb124db6
JungYoungSick <junfas1216@gmail.comgit config --global user.email junfas1216@gmail.com>
2023-10-18 11:45:29 +0900

    파이썬 주석 처리 완료(작동 확인(

--- Develop_1/app.py
+++ Develop_1/app.py
@@ -1,3 +1,4 @@
+# 파이썬에서의 반복문 작성.
 student_List = [
   "홍문기",
   "정영식",
@@ -5,10 +6,15 @@ student_List = [
   "유승민"
 ]
 
+# ":" 쌍점의 사용처 - 클래스 정의, 메서드 정의, 함수 정의, 조건문, 반복문 등에 쓰인다.
+#def는 함수란 뜻이며, :쌍점은 반복하라는 뜻이다.
 def develop_practics(array, name, start_text, end_text):
+  #반복문 
   for item in array:
+    # 조건문
     if item == name:
       print(f"{start_text}{item}{end_text}")
+    # 이외.
     else:
       print(item)
 
