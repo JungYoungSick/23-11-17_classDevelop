3ed98e89846f9fb7bdd194da49209920c3ff64f5
JungYoungSick <junfas1216@gmail.comgit config --global user.email junfas1216@gmail.com>
2023-10-18 17:26:37 +0900

    이름 변경 및 전체 백업

--- Develop_Training_11/Server.js
+++ Develop_Training_11/Server.js
@@ -9,22 +9,23 @@ const server = http.createServer((req, res) => {
 
   console.log(req.url ,req.method);
   if(req.url === '/' && req.method === 'GET') {
-    fs.readFile('app_T_11_1.html','utf8',(err,data) => {
+    fs.readFile('./Develop_Training_11/app_T_11_1.html','utf-8',(err,data) => {
       if(err) {
         serverErrerLog();
       }
-      res.writeHead(200,{'content-type':'text/html'});
+      res.writeHead(200,{'Content-Type':'text/html'});
       res.end(data);
     });
   }else if(req.url ==='app-T_11_1.CSS' && req.method === 'GET') {
-    fs.readFile('app-t_11_1.CSS','utf8',(err,data) => {
+    fs.readFile('./Develop_Training_11/app-T_11_1.CSS','utf-8',(err,data) => {
       if(err) {
         serverErrerLog();
       }
-      res.writeHead(205, {'content-type':'text/css'});
+      res.writeHead(200, {'Content-Type':'text/css'});
       res.end(data)
     })
   }
 })
-const port = 3000;
+const port = 8080;
 server.listen(port, () => {});
+
