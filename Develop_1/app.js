112527bffa47feabd5a1c3ea207ac42c90f6e23d
JungYoungSick <junfas1216@gmail.comgit config --global user.email junfas1216@gmail.com>
2023-10-18 10:53:45 +0900

    자바스크립트 매개변수를 이용한  for문 작성

--- Develop_1/app.js
+++ Develop_1/app.js
@@ -1,1 +1,18 @@
-aa
+const studentList = [
+  "홍문기",
+  "정영식",
+  "소사무엘",
+  "유승민"
+]
+
+
+
+function developPractics(array, name, startText, endText) {
+  for(let i=0; i<array.length; i++) {
+    if(array[i] === name) {
+      console.log(startText + array[i] + endText);
+    } else {
+      console.log(array[i])
+    }
+  }
+}
