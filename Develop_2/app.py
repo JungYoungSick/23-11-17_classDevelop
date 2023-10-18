3b161fa5c10bd2fa9ffe38277995ef08c2afe9f7
JungYoungSick <junfas1216@gmail.comgit config --global user.email junfas1216@gmail.com>
2023-10-18 12:01:44 +0900

    파이썬 객체 내용 정리.

---
+++ Develop_2/app.py
@@ -0,0 +1,15 @@
+# python의 매우 불편한 함수이며, format()함수를 특수한 경우에도 사용한다.
+# 읽히는 경험을 느껴보며 한번 작성해보자.
+dictionnary_example = {
+  "academyName": "그린컴퓨터아트학원",
+  "location":"둔산점",
+  "address": "대전광역시 서구 대덕대로 182 오라클 빌딩 3층",
+  "telNumber": "042-476-2111",
+  "classroomNumber": "it's different",
+  "studentList": [
+    "피카츄",
+    "라이츄",
+    "파이리",
+    "꼬부기"
+  ]
+}
