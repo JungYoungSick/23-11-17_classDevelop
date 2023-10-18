a20ade51160f3b22e5a83b9d3615cc0ae2c8cbe7
JungYoungSick <junfas1216@gmail.comgit config --global user.email junfas1216@gmail.com>
2023-10-18 16:33:16 +0900

    서버 html과 css 작동 코드 작성

---
+++ Develop_Training_11/Server.js
@@ -0,0 +1,27 @@
+const http = require('http')
+const fs = require('fs')
+
+const server = http.createServer((req, res) => {
+  function serverErrerLog() {
+    res.writeHead(500)
+    res.end("서버가 안돼요! 수정하세요");
+  }
+
+  console.log(req.url ,req.method);
+  if(req.url === '/' && req.method === 'GET') {
+    fs.readFile('app_T_11_1.html','utf8',(err,data) => {
+      if(err) {
+        serverErrerLog();
+      }
+      res.writeHead(200,{'content-type':'text/html'});
+      res.end(data);
+    });
+  }else if(req.url ==='app-T_11_1.CSS' && req.method === 'GET') {
+    fs.readFile('app-t_11_1.CSS','utf8',(err,data) => {
+      if(err) {
+        serverErrerLog();
+      }
+      res.writeHead(205, {'content-type':'text/css'});
+    })
+  }
+})
