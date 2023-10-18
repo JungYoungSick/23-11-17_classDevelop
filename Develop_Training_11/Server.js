8e4b1253cf12571d712af0ebdd3be392ae83f800
JungYoungSick <junfas1216@gmail.comgit config --global user.email junfas1216@gmail.com>
2023-10-18 19:00:23 +0900

    한글 오류 해결[readFile은상위 폴더에 점찍어야함]

--- Develop_Training_11/Server.js
+++ Develop_Training_11/Server.js
@@ -4,27 +4,30 @@ const fs = require('fs')
 const server = http.createServer((req, res) => {
   function serverErrerLog() {
     res.writeHead(500)
-    res.end("서버가 안돼요! 수정하세요");
+    return res.end("서버가 안돼요! 수정하세요");
   }
 
   console.log(req.url ,req.method);
   if(req.url === '/' && req.method === 'GET') {
-    fs.readFile('./Develop_Training_11/app_T_11_1.html',(err,data) => {
+    fs.readFile('./app_T_11_1.html',(err,data) => {
       if(err) {
         serverErrerLog();
       }
-      res.writeHead(200,"UTF-8",{'Content-Type':"text/html; charset=utf-8"})
+      res.writeHead(200, {'Content-Type':"text/html; charset=utf-8"})
       res.end(data);
     });
-  }else if(req.url ==='/app_T_11_1.CSS' && req.method === 'GET') {
-    fs.readFile('./Develop_Training_11/app_T_11_1.CSS',(err,data) => {
+  }
+  else if(req.url ==='/app_T_11_1.CSS' && req.method === 'GET') {
+    fs.readFile('./app_T_11_1.CSS',(err,data) => {
       if(err) {
         serverErrerLog();
       }
-      
       res.writeHead(200, {'Content-Type':'text/css; charset=utf-8'});
       res.end(data)
     })
+  } else {
+    res.writeHead(404);
+    res.end('Not found')
   }
 })
 const port = 3000;
