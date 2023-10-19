c9b859907592522fa84e3b60e3dae9ae99d24c82
JungYoungSick <junfas1216@gmail.comgit config --global user.email junfas1216@gmail.com>
2023-10-19 12:32:40 +0900

    app_T_11_1.js 변수 선언

---
+++ Develop_Training_11/app_T_11_1.js
@@ -0,0 +1,20 @@
+const ID = document.getElementById('id')
+const PW = document.getElementById('pw')
+const PW2 = document.getElementById('pw2')
+const EMAIL = document.getElementById('Email')
+
+
+
+
+// id는 소문자와 대문자가 포함되지 않으면 동작하지 않게 처리
+
+
+
+
+
+
+
+
+// Password 두개의 입력값이 같지 않으면 동작하지 않게 처리
+// email 입력 부분은 “@(atsign)”과, @이후 .(dot)이 존재하지 않으면 동작하지 않게 처리
+// 위 3개의 입력값이 모두 적절할때 아래 Sign up 버튼 동작
