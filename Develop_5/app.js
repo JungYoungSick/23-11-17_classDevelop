584789b2a5318939585f5141b000f13e770d96e8
JungYoungSick <junfas1216@gmail.comgit config --global user.email junfas1216@gmail.com>
2023-10-25 22:27:29 +0900

    객체 작성 및 타입검사 및 출력

---
+++ Develop_5/app.js
@@ -0,0 +1,10 @@
+var staticObject = {
+  name: "static object",
+  explain: "미리, 혹은 이미 작성된 개체",
+};
+//* 타입 검사를 통한 staticObject 확인.
+console.dir(typeof(staticObject));
+//* 출력으로 staticObject.name 키에 대한 값 출력.
+console.log(staticObject.name);
+//* 출력으로 staticObject.explain 키에 대한 값 출력.
+console.log(staticObject.explain);
