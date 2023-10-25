ba0da7bfbb1479eaa29ad0607589e4f648b79cf0
JungYoungSick <junfas1216@gmail.comgit config --global user.email junfas1216@gmail.com>
2023-10-25 22:33:02 +0900

    정적인 객체와 동적인 객체의 차이 비교

--- Develop_5/app.js
+++ Develop_5/app.js
@@ -8,3 +8,16 @@ console.dir(typeof(staticObject));
 console.log(staticObject.name);
 //* 출력으로 staticObject.explain 키에 대한 값 출력.
 console.log(staticObject.explain);
+
+//* dynamicObject를 객체형식으로 사용하겠다는 변수 선언.
+var dynamicObject = {};
+//* dynamicObject.name에 dynamic object라는 키값 입력.
+dynamicObject.name = "dynamic object";
+//* dynamicObject.explain에 동적으로 생성된 객체라는 키값 입력.
+dynamicObject.explain = "동적으로 생성된 객체";
+//* 타입 검사를 통한  dynamicObject 확인.
+console.dir(typeof(dynamicObject));
+//* 출력으로 dynamicObject.name 키에 대한 값 출력.
+console.log(dynamicObject.name);
+//* 출력으로 dynamicObject.explain 키에 대한 값 출력.
+console.log(dynamicObject.explain);
