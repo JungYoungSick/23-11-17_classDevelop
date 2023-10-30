e40099fb6e4832ca79f488c69110f7d4d765293b
JungYoungSick <junfas1216@gmail.com>
2023-10-30 21:17:12 +0900

    [this.port]생성자 함수를 사용한 서버 생성

--- Develop_22/app.js
+++ Develop_22/app.js
@@ -1,1 +1,29 @@
-aa
+const http = require('http');
+
+class SimpleServer {
+  constructor(port) {
+    this.port = port;
+  }
+  start() {
+    const server =http.createServer((req,res)=>{
+      res.writeHead(200, {'Content-Type' : 'text/plain'});
+      res.end('생성자 함수로 가동된 서버입니다.')
+    });
+    server.removeListener(this.port, () => {
+      console.log(`http://localhost:${this.port}`);
+    });
+  }
+}
+
+const simpleApp =new SimpleServer(3000);
+simpleApp.start();
+
+const portRange = {
+  min:3001,
+  max:3005,
+}
+
+for(let i = portRange.min; i<portRange.max; i++) {
+  const app = new SimpleServer(i);
+  app.start()
+}
