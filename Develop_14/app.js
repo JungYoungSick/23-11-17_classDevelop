19be0a2063fdbbad382db7033daa4df1ec30f0e6
JungYoungSick <junfas1216@gmail.com>
2023-11-05 21:26:01 +0900

    Develop_14 무엇을 '만들어'서 전달할까? serving. 작성 완료

---
+++ Develop_14/app.js
@@ -0,0 +1,33 @@
+const http = require("http")
+
+const server = http.createServer(function (require, response) {
+  console.log(require.url);
+
+  function docMaker(title, href, test) {
+    return `
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>${title}</title>
+</head>
+<body>
+  <a href="${href}">${test}</a>
+</body>
+</html>
+`;
+  }
+  if (require.url === "/") {
+    let doc = docMaker("메인페이지", "/sub", "서브페이지로 이동!");
+    response.writeHead(200, { "content-Type": "text/html" });
+    response.end(doc);
+  }
+  if (require.url === "/sub") {
+    let doc = docMaker("서브페이지", "/sub", "메인페이지로 이동!");
+    response.writeHead(200, { "content-Type": "text/html" });
+    response.end(doc);
+  }
+})
+
+server.listen(8080);
