c15d62cc85143c4711f70e833ec0325922f7ee04
JungYoungSick <junfas1216@gmail.com>
2023-11-08 00:55:50 +0900

    Develop_20 객체를 만들때 필요한 만큼 '셋팅'할 수 있다면? 작성 완료

---
+++ Develop_20/app.js
@@ -0,0 +1,17 @@
+class MakeInstance {
+  constructor(idValue, passwordValue, emailValue) {
+    this.id = idValue;
+    this.password = passwordValue;
+    this.email = emailValue;
+  }
+  set id(value) {
+    if (typeof (value) === "string") {
+      this._id = value + "님";
+    } else {
+      console.log("id는 문자열이어야 합니다.");
+    }
+  }
+}
+
+const instance = new MakeInstance('test', 'password', 'email');
+console.log(instance)
